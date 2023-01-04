37408725098550d48603a391fb0669917fb42457
jh0326 <youngtong111@naver.com>
2023-01-04 21:31:53 +0900

    회원정보예제연습

---
+++ src/회원정보입력하기/MemberInfo.java
@@ -0,0 +1,12 @@
+package 회원정보입력하기;
+//
+import java.util.Scanner;
+
+public class MemberInfo {
+    public static void main(String[] args) {
+        Member member =  new Member();
+        member.setName();
+        member.setAge();
+        member.setGender();
+    }
+}
